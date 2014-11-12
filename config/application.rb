27803ea91ff13746f80315093a23098dc4f4a7f5
things23 <twothreethings@gmail.com>
2014-11-12 17:52:53 +0400

    Refactor

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ module Flashcardr
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-      config.autoload_paths += %W["#{config.root}/app/validators/"]
+    config.autoload_paths += %W["#{config.root}/app/validators/"]
   end
 end
