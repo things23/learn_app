6556b44d111d560819e6c7eb3fcd38ad078aeb2b
things23 <twothreethings@gmail.com>
2014-11-12 06:05:51 +0400

    Add create, edit and delete actions. Add validations. Create links, views. Make forms with simple_form gem.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,46 @@
 class CardsController < ApplicationController
+  before_action :find_card, only: [:edit, :update, :destroy]
   def index
     @cards = Card.all
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def create
+    @card = Card.new(cards_params)
+
+    if @card.save
+      redirect_to cards_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @card.update(cards_params)
+      redirect_to cards_path
+    else
+     render 'edit'
+   end
+  end
+
+  def destroy
+    @card.destroy
+
+    redirect_to cards_path
+  end
+
+  private
+    def find_card
+      @card = Card.find(params[:id])
+    end
+
+    def cards_params
+      params.require(:card).permit(:original_text, :translated_text, :review_date)
+    end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,18 @@
 class Card < ActiveRecord::Base
+  validates :original_text, presence: true, uniqueness: true
+  validates :translated_text, presence: true
+
+  before_validation :ensure_not_the_same
+
+  protected
+
+    def ensure_not_the_same
+      if self.original_text.downcase != self.translated_text.downcase
+        true
+      else
+        errors.add(:base, "Original text and translated must be different")
+        false
+      end
+
+  end
 end
