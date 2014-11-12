a533c3977e574ff7a3a855a6152946abb8415b92
things23 <twothreethings@gmail.com>
2014-11-12 19:00:03 +0400

    Refactor custom validation, css style sheets, .gitignore

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,11 @@
+class DifferenceValidator < ActiveModel::Validator
+  def validate(record)
+    if record.original_text.downcase == record.translated_text.downcase
+      record.errors.add(:base, "Original text and translated must be different")
+    end
+  end
+end
+
 class Card < ActiveRecord::Base
   validates :original_text, presence: true, uniqueness: true
   validates :translated_text, presence: true
