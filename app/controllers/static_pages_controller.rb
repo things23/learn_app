5f3e9a89bd5d7845cd78a86212a67c83f2637955
things23 <twothreethings@gmail.com>
2014-11-15 07:00:28 +0400

    Final refactor

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,14 +4,13 @@ class StaticPagesController < ApplicationController
   end
 
   def review_card
-    @card = Card.find_by(id: params[:card_id])
+    @card = Card.find(params[:card_id])
     if @card.check_answer(params[:translation])
       @card.change_review_date
       flash[:right] = "Правильный ответ"
-      redirect_to root_path
     else
       flash[:wrong] = "Неправильный ответ"
-      redirect_to root_path
     end
+    redirect_to root_path
   end
 end
