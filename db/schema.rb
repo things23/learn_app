73d61719b16d2fe890f340429299d8bf205458c0
things23 <twothreethings@gmail.com>
2014-11-19 18:21:37 +0400

    Change name of users atrtribute form password_digest to password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119111725) do
+ActiveRecord::Schema.define(version: 20141119141800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20141119111725) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password_digest"
+    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
