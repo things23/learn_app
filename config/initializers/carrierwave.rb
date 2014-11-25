98350610718213805041548299b76bbdc0e52ff7
things23 <twothreethings@gmail.com>
2014-11-25 23:14:06 +0100

    Refactor carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,5 +5,17 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: ENV["AWS_S3_REGION"]
   }
+
+  if Rails.env.test? || Rails.env.development?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
+  else
+    config.storage = :fog
+  end
+
+  config.cache_dir = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
   config.fog_directory  = ENV["AWS_S3_BUCKET"]
+  config.fog_public     = false
+  config.fog_attributes = {}
 end
