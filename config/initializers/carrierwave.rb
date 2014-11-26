ba497440c590674ed500cd82da74d84e20641844
things23 <twothreethings@gmail.com>
2014-11-26 13:49:29 +0100

    Refactor

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,5 +5,5 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
     region: ENV["AWS_S3_REGION"]
   }
-  config.fog_directory  = ENV["AWS_S3_BUCKET"]
+  config.fog_directory = ENV["AWS_S3_BUCKET"]
 end
