b645edc861e5f6b31412b84924834e49ba9d553a
things23 <twothreethings@gmail.com>
2014-11-26 18:04:33 +0100

    Refactor carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,16 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: Rails.application.secrets.aws_region
-  }
-
-
   if Rails.env.test? || Rails.env.development?
     config.storage = :file
     config.enable_processing = false
     config.root = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
   else
     config.storage = :fog
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: Rails.application.secrets.aws_region
+    }
+    config.fog_directory = Rails.application.secrets.aws_bucket
   end
-
-  config.fog_directory = Rails.application.secrets.aws_bucket
 end
